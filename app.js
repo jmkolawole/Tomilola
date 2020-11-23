c2452fe1d54287f60ba8a52d10b938853d1b5310
korlahwarleh <jmkolawole@gmail.com>
2020-11-23 13:11:56 +0100

    view files again

--- app.js
+++ app.js
@@ -66,6 +66,14 @@ next();
 
 });
 
+if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
+  app.use(express.static('frontend/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/frontend/build/index.html'));
+  });
+} 
+
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));

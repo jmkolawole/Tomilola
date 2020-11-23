d22be98cdaf83fe01192a8c77d3a64a235be89b2
korlahwarleh <jmkolawole@gmail.com>
2020-11-23 13:59:17 +0100

    server

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var app = express();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/frontend/build/index.html'));
+  });
 });
 
 module.exports = router;
